aeb33d4eb5dfaeb6a3ce6430619c211eab5f862c
Vansh Jain <vanshj9971@gmail.com>
2026-05-11 22:57:31 +0530

    first commit

--- 03_database/src/db/db.js
+++ 03_database/src/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 async function connectDB(){
 
-    await mongoose.connect("mongodb+srv://uesr-1:xNJ14UFjhHIJQIAa@backend-study.6fi0mdb.mongodb.net/halley");
+    await mongoose.connect(`mongodb+srv://uesr-1:${process.env.DB_PASSWORD}@backend-study.6fi0mdb.mongodb.net/halley`);
     console.log("Connected to DB");
 }
 
